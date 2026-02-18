2b298f91ba133e4b66161c63bdd9becf1a65ca28
quanbui <quanbv@vnext.vn>
2026-02-18 17:03:08 +0900

    Initial commit

---
+++ src/InfraCdk/InfraCdkStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace InfraCdk
+{
+    public class InfraCdkStack : Stack
+    {
+        internal InfraCdkStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
